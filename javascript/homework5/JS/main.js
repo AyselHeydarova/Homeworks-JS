76afa3e415fa43bd482e643cec26ac2a9884430c
Aysel <aysel@MacBook-Pro-Aysel.local>
2020-02-26 01:33:58 +0400

    JS HW5 Done

--- javascript/homework5/JS/main.js
+++ javascript/homework5/JS/main.js
@@ -1,66 +1,24 @@
 
 
-
-// let tabItem = document.getElementsByClassName('tabs-title');
-// let tabsContent = document.getElementsByClassName('tabs-content');
-//
-// tabs.addEventListener('click', event=> {
-//     let pressedTab = event.target;
-//     pressedTab.classList.add('active');
-//     // let contentIndex = tabs.indexOf(pressedTab);
-//     let contentItem = document.getElementsByClassName('content-item');
-//     contentItem.style.display = 'block'
-//
-// });
-
 let tabs = document.getElementById('tabs');
+let tabItem = document.getElementsByClassName('tabs-title');
+let contentItem = document.getElementsByClassName('content-item');
+let contentItemArray = [...contentItem];
+let tabItemArray = [...tabItem];
 
 tabs.addEventListener('click', event=> {
-    let tabItem = document.getElementsByClassName('tabs-title');
-    let tabsContent = document.getElementsByClassName('tabs-content');
-    let pressedIndex = null;
 
-    tabItem[pressedIndex] = event.target;
+    let pressedItem = event.target;
+    let openTabIndex = tabItemArray.indexOf(pressedItem);
 
-    if (tabItem[pressedIndex] === event.target) {
-        tabItem[pressedIndex].classList.add('active');
-        tabsContent[pressedIndex].style.display = "block";
+    for (let i = 0; i < tabItemArray.length; i++) {
+        if (i === openTabIndex) {
+            tabItemArray[openTabIndex].classList.add('active');
+            contentItemArray[openTabIndex].style.display = "block";
+        } else {
+            tabItemArray[i].classList.remove('active');
+            contentItemArray[i].style.display = 'none';
+        }
     }
-        for (let i = 0; i < tabsContent.length; i++){
-        tabItem.classList.replace('active', '');
-        tabsContent.style.display = "none";
-    }
-
 });
 
-
-// pressedTab.classList.add('active');
-//
-//
-// let tabsContent = document.getElementsByClassName('tabs-content');
-// for (let i = 0; i < tabsContent.length; i++) {
-//     tabsContent[i].style.display = "none";
-// }
-// let tabItem = document.getElementsByClassName('tabs-title');
-// for (let i = 0; i < tabItem.length; i++) {
-//     tabItem[i].className = tabItem[i].className.replace(" active", "");
-// }
-// // let contentIndex = tabs.indexOf(pressedTab);
-// let contentItem = document.getElementsByClassName('content-item');
-// contentItem.style.display = 'block'
-// function openTabs(event, text) {
-//     let tabsContent = document.getElementsByClassName('tabs-content');
-//     for (let i = 0; i < tabsContent.length; i++) {
-//         tabsContent[i].style.display = "none";
-//     }
-//
-//     let tabItem = document.getElementsByClassName('tabs-title');
-//     for (let i = 0; i < tabItem.length; i++) {
-//         tabItem[i].className = tabItem[i].className.replace(" active", "");
-// }
-// //     // Show the current tab, and add an "active" class to the button that opened the tab
-// //     document.getElementById(cityName).style.display = "block";
-// //     evt.currentTarget.className += " active";
-// // }
-//
-// }
